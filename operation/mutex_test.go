621ffa0570a178d748a233b83762adf7a278419a
lucas-masiero <lucasmasiero38@gmail.com>
2024-12-10 17:48:37 -0300

    ✅ test (operation/mutex_test.go): added test for Done() method

--- operation/mutex_test.go
+++ operation/mutex_test.go
@@ -56,3 +56,48 @@ func TestAtomicMutex_Do(t *testing.T) {
 		}
 	})
 }
+
+func TestAtomicMutex_Done(t *testing.T) {
+	t.Run("success to share an only-read channel, done is prepare", func(t *testing.T) {
+		am := &AtomicMutex{
+			fn:   nil,
+			mu:   sync.Mutex{},
+			done: make(chan struct{}),
+		}
+
+		ch, err := am.Done()
+
+		if err != nil {
+			t.Errorf("expected no error, got %s", err.Error())
+			return
+		}
+		expected := (<-chan struct{})(am.done)
+		if expected != ch {
+			t.Errorf("expected channel %v, got %v", expected, ch)
+			return
+		}
+	})
+
+	t.Run("failure to share an only-read channel, done is not prepared", func(t *testing.T) {
+		am := &AtomicMutex{
+			fn:   nil,
+			mu:   sync.Mutex{},
+			done: nil,
+		}
+
+		ch, err := am.Done()
+
+		if err == nil {
+			t.Error("expected error, got nil")
+			return
+		}
+		if err != ErrMutexChannelUnready {
+			t.Errorf("expected err %s, got %s", ErrMutexChannelUnready, err.Error())
+			return
+		}
+		if ch != nil {
+			t.Errorf("expected channel to be nil, got %v", ch)
+			return
+		}
+	})
+}
