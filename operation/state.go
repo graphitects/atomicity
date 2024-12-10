9f8a49ca37eafe78b3e9e7c88de4ce31a4e51897
lucas-masiero <lucasmasiero38@gmail.com>
2024-12-10 16:57:09 -0300

    ✨ feat(operation): added state implementation to handle atomic operations with signaling

---
+++ operation/state.go
@@ -0,0 +1,56 @@
+package operation
+
+import (
+	"errors"
+	"sync/atomic"
+)
+
+var (
+	ErrStateDoUnavailable  = errors.New("operation is not available")
+	ErrStateChannelUnready = errors.New("channel is not prepared")
+)
+
+type AtomicState struct {
+	fn    func()
+	state uint32
+	done  chan struct{} // to signal when the operation is done
+}
+
+func (am *AtomicState) Do() error {
+	if !atomic.CompareAndSwapUint32(&am.state, 0, 1) {
+		return ErrStateDoUnavailable
+	}
+	defer atomic.StoreUint32(&am.state, 0)
+
+	// prepare the channel
+	am.done = make(chan struct{})
+	defer close(am.done)
+
+	am.fn()
+	return nil
+}
+
+func (am *AtomicState) DoAsync() error {
+	if !atomic.CompareAndSwapUint32(&am.state, 0, 1) {
+		return ErrStateDoUnavailable
+	}
+
+	go func() {
+		defer atomic.StoreUint32(&am.state, 0)
+		defer close(am.done)
+
+		// prepare channel
+		am.done = make(chan struct{})
+		am.fn()
+	}()
+
+	return nil
+}
+
+func (am *AtomicState) Done() (<-chan struct{}, error) {
+	if am.done == nil {
+		return nil, ErrStateChannelUnready
+	}
+
+	return am.done, nil
+}
