08ffc48488f47c6f09c4829803ee346d3e4483ef
lucas-masiero <lucasmasiero38@gmail.com>
2024-12-10 20:28:25 -0300

    ✅ test(operation/state_integration_test.go): added integration test for AtomicState on its methods DoAsync() and Done()

--- operation/state_integration_test.go
+++ operation/state_integration_test.go
@@ -58,3 +58,49 @@ func TestAtomicState_Do_Done(t *testing.T) {
 		}
 	})
 }
+
+func TestAtomicState_DoAsync_Done(t *testing.T) {
+	t.Run("success to execute DoAsync() and receive the signal from Done()", func(t *testing.T) {
+		as := &AtomicState{
+			fn:    func() { time.Sleep(time.Second) },
+			state: 0,
+			done:  nil,
+		}
+
+		err := as.DoAsync()
+		if err != nil {
+			t.Errorf("expected no error, got %s", err.Error())
+			return
+		}
+
+		time.Sleep(time.Millisecond * 100)
+
+		subs := [2]<-chan struct{}{nil, nil}
+		for i := 0; i < 2; i++ {
+			var err error
+			subs[i], err = as.Done()
+			if err != nil {
+				t.Errorf("expected no error, got %s", err.Error())
+				return
+			}
+		}
+
+		for i := 0; i < 2; i++ {
+			select {
+			case <-time.After(time.Second):
+				t.Error("expected channel to be closed, but did not")
+				return
+			case <-subs[i]:
+				// success
+			}
+		}
+
+		select {
+		case <-time.After(time.Millisecond * 100):
+			t.Error("expected main call to be done, but did not")
+			return
+		case <-as.done:
+			// success
+		}
+	})
+}
