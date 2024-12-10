9c33731cf7b708817599abf9aed0a46222e5fdba
lucas-masiero <lucasmasiero38@gmail.com>
2024-12-10 19:52:48 -0300

    ✅ test(operation/state_test.go): added test to method Done() cases: - sucess with a prepared channel - failure with a nil channel

--- operation/state_test.go
+++ operation/state_test.go
@@ -102,3 +102,48 @@ func TestAtomicState_DoAsync(t *testing.T) {
 		}
 	})
 }
+
+func TestAtomicState_Done(t *testing.T) {
+	t.Run("success to share an only-read channel, done is prepare", func(t *testing.T) {
+		am := &AtomicState{
+			fn:    nil,
+			state: 0,
+			done:  make(chan struct{}),
+		}
+
+		ch, err := am.Done()
+
+		if err != nil {
+			t.Errorf("expected no error, got %s", err.Error())
+			return
+		}
+		expected := (<-chan struct{})(am.done)
+		if expected != ch {
+			t.Errorf("expected channel %v, got %v", expected, ch)
+			return
+		}
+	})
+
+	t.Run("failure to share an only-read channel, done is not prepared", func(t *testing.T) {
+		am := &AtomicState{
+			fn:    nil,
+			state: 0,
+			done:  nil,
+		}
+
+		ch, err := am.Done()
+
+		if err == nil {
+			t.Error("expected error, got nil")
+			return
+		}
+		if err != ErrStateChannelUnready {
+			t.Errorf("expected err %s, got %s", ErrStateChannelUnready, err.Error())
+			return
+		}
+		if ch != nil {
+			t.Errorf("expected channel to be nil, got %v", ch)
+			return
+		}
+	})
+}
